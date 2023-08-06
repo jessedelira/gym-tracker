b70c05ad54d2d83009f7df4faca73572a48c74bc
Jesse De Lira <jessedelira1@gmail.com>
2023-08-05 23:10:29 -0700

    Updated Icons
    This is will make a more cohesive ui that users will have the same ui across the applicaiton

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,20 +23,33 @@ const NavBar: React.FC<NavBarProps> = ({ sessionData }) => {
 		<nav className="flex h-[4rem] w-full flex-row items-center justify-between border-b-4 border-b-black bg-white">
 			<div className="flex flex-row items-center justify-between gap-4">
 				<Link href="/home" className="ml-3 rounded-full text-5xl">
-					🏠
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						className="h-6 w-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+						/>
+					</svg>
 				</Link>
 			</div>
 
 			<div className="flex flex-col gap-1">
 				<p className="text-1xl text-center text-black">
-					{sessionData && sessionData.user?.firstName}&apos;s Workout
+					Welcome, {sessionData?.user?.firstName}
 				</p>
-				<p className="text-1xl text-center text-black">{getDate()}</p>
+				
 			</div>
 
 			<div className="flex flex-row items-center justify-between gap-4">
 				<button
-					className="ml-3 rounded-full text-5xl "
+					className=" mr-3 rounded-full text-5xl"
 					onClick={() => void signOut()}
 				>
 					<svg
