59ab3d881fbe6dd06364815f2b65e7e91ad438ae
Jesse De Lira <jessedelira1@gmail.com>
2023-09-21 14:56:22 -0700

    formatting/adding to the create screen

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,10 +1,15 @@
 import { type NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import NavBar from '~/components/navbar';
 
 const Account: NextPage = () => {
+	const { data: sessionData, status } = useSession();
+
 	return (
-		<div>
+		<>
+			<NavBar sessionData={sessionData}></NavBar>
 			<h1>Account</h1>
-		</div>
+		</>
 	);
 };
 
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -3,22 +3,22 @@ import NavBar from '~/components/navbar';
 import { useSession } from 'next-auth/react';
 
 const Create: NextPage = () => {
-
-    const { data: sessionData, status } = useSession();
+	const { data: sessionData, status } = useSession();
 
 	return (
 		// create three buttons: create workout, create exercise, create routine with tailwind css
 		<>
-            <NavBar sessionData={sessionData}></NavBar>
+			<NavBar sessionData={sessionData}></NavBar>
 			<div className="flex flex-col items-center justify-center">
 				<h1 className="text-2xl font-bold">Create</h1>
-				<button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+				{/* create buttons that match the rest of the buttons of white and black and grey with routines, sessions, workouts */}
+				<button className="mt-4 rounded-md bg-black px-4 py-2 text-white">
 					Create Workout
 				</button>
-				<button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+				<button className="mt-4 rounded-md bg-black px-4 py-2 text-white">
 					Create Exercise
 				</button>
-				<button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+				<button className="mt-4 rounded-md bg-black px-4 py-2 text-white">
 					Create Routine
 				</button>
 			</div>
@@ -27,5 +27,3 @@ const Create: NextPage = () => {
 };
 
 export default Create;
-
-
