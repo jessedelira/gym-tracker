ed1397669b816721c18fdf68cdb2cb292f83e29d
Jesse De Lira <jessedelira1@gmail.com>
2023-10-06 14:48:23 -0700

    try adding manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from 'next-auth/react';
 import { api } from '~/utils/api';
 
 import '~/styles/globals.css';
+import Head from 'next/head';
 
 const MyApp: AppType<{ session: Session | null }> = ({
 	Component,
@@ -12,6 +13,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
 	return (
 		<SessionProvider session={session}>
+			<Head>
+        <title>My Page Title</title>
+        <meta name="description" content="My page description" />
+		<link rel="manifest" href="/manifest.json"></link>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
