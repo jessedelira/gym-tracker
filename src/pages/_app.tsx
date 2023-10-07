eae031b7256d435b8da790e084d85159d2997cfd
Jesse De Lira <jessedelira1@gmail.com>
2023-10-06 23:15:33 -0700

    remove import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { SessionProvider } from 'next-auth/react';
 import { api } from '~/utils/api';
 
 import '~/styles/globals.css';
-import Head from 'next/head';
 
 const MyApp: AppType<{ session: Session | null }> = ({
 	Component,
