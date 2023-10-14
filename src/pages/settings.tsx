edf6000337b322319b626845754b172edb1993cb
Jesse De Lira <jessedelira1@gmail.com>
2023-10-13 17:52:59 -0700

    Feat: save & cancel buttons show with form changes

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,3 +1,4 @@
+import { User } from '@prisma/client';
 import { type NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
@@ -7,7 +8,13 @@ import NavBar from '~/components/navbar';
 const Settings: NextPage = () => {
 	const { data: sessionData, status } = useSession();
 	const [isLoading, setIsLoading] = useState(true);
+	const [userDto,setUserDto] = useState<User>();
+	const [dataChangeInForm, setDataChangeInForm] = useState(false);
 	const router = useRouter();
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setDataChangeInForm(true);
+	}
 
 	useEffect(() => {
 		if (status === 'unauthenticated') {
@@ -25,37 +32,39 @@ const Settings: NextPage = () => {
 		return (
 			<>
 				<NavBar sessionData={sessionData}></NavBar>
-				{/* create a account settings page with inputs for first name, last name, username, and password*/}
 				<div className="flex flex-col items-center justify-center">
 					<h1 className="text-2xl font-bold">Account Settings</h1>
-					{/* create inputs that match the rest of the inputs of white and black and grey with routines, sessions, workouts */}
+
 					<input
 						className="mt-4 rounded-md bg-black px-4 py-2 text-white"
 						placeholder="First Name"
-						value={sessionData?.user.firstName ?? 'Loading...'}
+						defaultValue={sessionData?.user.firstName ?? 'Loading...'}
+						onChange={handleInputChange}
 					></input>
 					<input
 						className="mt-4 rounded-md bg-black px-4 py-2 text-white"
 						placeholder="Last Name"
-						value={sessionData?.user.lastName ?? 'Loading...'}
+						defaultValue={sessionData?.user.lastName ?? 'Loading...'}
+						onChange={handleInputChange}
 					></input>
 					<input
 						className="mt-4 rounded-md bg-black px-4 py-2 text-white"
 						placeholder="Username"
-						value={sessionData?.user.username ?? 'Loading...'}
+						defaultValue={sessionData?.user.username ?? 'Loading...'}
+						onChange={handleInputChange}
 					></input>
 					<input
 						className="mt-4 rounded-md bg-black px-4 py-2 text-white"
 						placeholder="Password"
-					></input>	
-					<div className="mt-4 grid grid-col-2 gap-1">
-						<button className="rounded-md bg-black px-4 py-2 text-white">
-							Submit
-						</button>
-						<button className="rounded-md bg-black px-4 py-2 text-white">
-							Cancel{' '}
-						</button>
-					</div>
+					></input>
+					{dataChangeInForm ? (
+						<div className="mt-4 grid grid-col-2 gap-1">
+							<button className="rounded-md bg-black px-4 py-2 text-white">Save</button>
+							<button className="rounded-md bg-black px-4 py-2 text-white">Cancel</button>
+						</div>
+
+					) : null}
+
 				</div>
 			</>
 		);
