16ee36bf1464766afbc240da2f525eab51b080b2
Jesse De Lira <jessedelira1@gmail.com>
2023-10-17 07:08:43 +0900

    added a settings page and display user info

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -3,11 +3,8 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import NavBar from '~/components/navbar';
-import {
-	getFirstNameInputElement,
-	getLastNameInputElement,
-	getUsernameInputElement,
-} from '~/utils/documentUtils';
+import Account from './account';
+import { getMonthDDCommaYYYY, getYYYY } from '~/utils/dateUtils';
 
 const Settings: NextPage = () => {
 	const { data: sessionData, status } = useSession();
@@ -22,7 +19,7 @@ const Settings: NextPage = () => {
 		} else {
 			setIsLoading(false);
 		}
-	}, [status, router]);
+	}, [status, router, sessionData?.user]);
 
 	if (isLoading) {
 		return <></>;
@@ -31,8 +28,31 @@ const Settings: NextPage = () => {
 			<>
 				<NavBar sessionData={sessionData}></NavBar>
 				{/* name and 'member since xyx' */}
-				<div className=''>
 
+				<div className="flex items-center ">
+
+					<div className='pl-4 mt-4'>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="h-12 w-12"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+							/>
+						</svg>
+					</div>
+					<div className="ml-4 grid grid-cols-1 mt-4">
+						<h1 className='text-2xl'>
+							{sessionData?.user.firstName} {sessionData?.user.lastName}
+						</h1>
+						<h2 className='text-l'>Member since {getMonthDDCommaYYYY(sessionData?.user.dateCreated)}</h2>
+					</div>
 				</div>
 
 				{/* Account */}
