11abe7854c829e55cc66940578fd3b48aff0fc5b
Jesse De Lira <jessedelira1@gmail.com>
2023-10-18 07:42:52 +0900

    deletion icon

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -167,11 +167,12 @@ const Settings: NextPage = () => {
 							className="ml-0.5 h-9 w-7 flex-none pb-1"
 						>
 							<path
-								fillRule="evenodd"
-								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
-								clipRule="evenodd"
+								fill-rule="evenodd"
+								d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+								clip-rule="evenodd"
 							/>
 						</svg>
+
 						<Link
 							href="/settings/account"
 							className="h-9 w-14 flex-initial whitespace-nowrap  pl-3 text-2xl"
