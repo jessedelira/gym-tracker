212250dff1d83608adfab142dfce5e738b8c85fe
Jesse De Lira <jessedelira1@gmail.com>
2023-10-18 07:17:17 +0900

    created styling for the index of the setting pages

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -29,7 +29,7 @@ const Settings: NextPage = () => {
 				<NavBar sessionData={sessionData}></NavBar>
 				{/* name and 'member since xyx' */}
 
-				<div className="flex items-center ">
+				<div className="flex items-center">
 					<div className="mt-4 pl-4">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -58,51 +58,127 @@ const Settings: NextPage = () => {
 					</div>
 				</div>
 
-				<div className="ml-4 mr-4 rounded-md border-2 mt-5 border-slate-400 bg-gray-100">
-					<div className=''>
+				<div className="ml-4 mr-4 mt-5 rounded-md border-2 border-slate-400 bg-gray-100">
+					<div className="flex h-8 border-b-2 border-black">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="h-9 w-7 flex-none pb-1 ml-0.5"
+						>
+							<path
+								fillRule="evenodd"
+								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+								clipRule="evenodd"
+							/>
+						</svg>
 						<Link
 							href="/settings/account"
-							className="block border-b-2 border-black text-2xl"
+							className="h-9 w-14 flex-initial  pl-3 text-2xl"
 						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-								className="h-5 w-5"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
-									clip-rule="evenodd"
-								/>
-							</svg>
 							Account
 						</Link>
 					</div>
-					<Link
-						href="/settings/appearance"
-						className="block border-b-2 border-black text-2xl"
-					>
-						Appearance
-					</Link>
-					<Link
-						href="/settings/location"
-						className="block border-b-2 border-black text-2xl"
-					>
-						Location & Language
-					</Link>
-					<Link
-						href="/settings/notifications"
-						className="block border-b-2 border-black text-2xl"
-					>
-						Notifications
-					</Link>
-					<Link
-						href="/settings/delete"
-						className="block border-b-2 text-2xl "
-					>
-						Account Deletion
-					</Link>
+					<div className="flex h-8 border-b-2 border-black">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="h-9 w-7 flex-none pb-1 ml-0.5"
+						>
+							<path
+								fillRule="evenodd"
+								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+								clipRule="evenodd"
+							/>
+						</svg>
+						<Link
+							href="/settings/account"
+							className="h-9 w-14 flex-initial  pl-3 text-2xl"
+						>
+							Account
+						</Link>
+					</div>
+					<div className="flex h-8 border-b-2 border-black">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="h-9 w-7 flex-none pb-1 ml-0.5"
+						>
+							<path
+								fillRule="evenodd"
+								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+								clipRule="evenodd"
+							/>
+						</svg>
+						<Link
+							href="/settings/account"
+							className="h-9 w-14 flex-initial  pl-3 text-2xl"
+						>
+							Appearance
+						</Link>
+					</div>
+					<div className="flex h-8 border-b-2 border-black">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="h-9 w-7 flex-none pb-1 ml-0.5"
+						>
+							<path
+								fillRule="evenodd"
+								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+								clipRule="evenodd"
+							/>
+						</svg>
+						<Link
+							href="/settings/account"
+							className="h-9 w-14 flex-initial  pl-3 text-2xl"
+						>
+							Location & Language
+						</Link>
+					</div>
+					<div className="flex h-8 border-b-2 border-black">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="h-9 w-7 flex-none pb-1 ml-0.5"
+						>
+							<path
+								fillRule="evenodd"
+								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+								clipRule="evenodd"
+							/>
+						</svg>
+						<Link
+							href="/settings/account"
+							className="h-9 w-14 flex-initial  pl-3 text-2xl"
+						>
+							Notifications
+						</Link>
+					</div>
+					<div className="flex h-8">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="h-9 w-7 flex-none pb-1 ml-0.5"
+						>
+							<path
+								fillRule="evenodd"
+								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+								clipRule="evenodd"
+							/>
+						</svg>
+						<Link
+							href="/settings/account"
+							className="h-9 w-14 flex-initial  pl-3 text-2xl"
+						>
+							Account Deletion
+						</Link>
+					</div>
 				</div>
 			</>
 		);
