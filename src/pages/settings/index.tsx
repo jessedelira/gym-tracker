a59e00d673b6ffd3403238f257f6b2e37a9328aa
Jesse De Lira <jessedelira1@gmail.com>
2023-10-19 08:46:20 +0900

    adding example how to use Link tag for account

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -57,26 +57,25 @@ const Settings: NextPage = () => {
 				</div>
 
 				<div className="ml-4 mr-4 mt-5 rounded-md border-2 border-slate-400 bg-gray-100">
-					<div className="flex h-8 border-b-2 border-black">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							className="ml-0.5 h-9 w-7 flex-none pb-1"
-						>
-							<path
-								fillRule="evenodd"
-								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
-								clipRule="evenodd"
-							/>
-						</svg>
-						<Link
-							href="/settings/account"
-							className="h-9 w-14 flex-initial  pl-3 text-2xl"
-						>
-							Account
-						</Link>
-					</div>
+					<Link href="/settings/account">
+						<div className="flex h-8 border-b-2 border-black">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								className="ml-0.5 h-9 w-7 flex-none pb-1"
+							>
+								<path
+									fillRule="evenodd"
+									d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+									clipRule="evenodd"
+								/>
+							</svg>
+							<p className="h-9 w-14 flex-initial  pl-3 text-2xl">
+								Account
+							</p>
+						</div>
+					</Link>
 					<div className="flex h-8 border-b-2 border-black">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
