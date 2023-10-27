8524a6c8da34b6162f0f1950344df79811aeb65a
Jesse De Lira <jessedelira1@gmail.com>
2023-10-27 12:03:44 +0900

    new route + added placeholder data

---
+++ src/pages/create/session.tsx
@@ -0,0 +1,34 @@
+import { type NextPage } from 'next';
+import NavBar from '~/components/navbar';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const Session: NextPage = () => {
+    const {data: sessionData, status } = useSession();
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === 'unauthenticated') {
+            void router.push('/');
+        } else if (status === 'loading') {
+            setIsLoading(true);
+        } else {
+            setIsLoading(false);
+        }
+    }, [status, router]);
+
+    if (isLoading) {
+        return <></>;
+    } else {
+        return (
+            <>
+                <h1>Create Workout Page</h1>
+            </>
+        )
+    }
+}
+
+export default Session;
