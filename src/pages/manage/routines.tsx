b95ce182785f618f770d213d1dbb068e58ed079e
Jesse De Lira <jessedelira1@gmail.com>
2023-10-29 20:51:11 +0900

    removing duplicate table code

--- src/pages/manage/routines.tsx
+++ src/pages/manage/routines.tsx
@@ -67,39 +67,7 @@ const Routines: NextPage = () => {
 							</tr>
 						</thead>
 						<tbody>
-							<tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-								<th
-									scope="row"
-									className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-								>
-									Apple MacBook Pro 17
-								</th>
-								<td className="px-6 py-4 text-right">
-									<a
-										href="#"
-										className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-									>
-										Edit
-									</a>
-								</td>
-							</tr>
-							<tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-								<th
-									scope="row"
-									className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-								>
-									Microsoft Surface Pro
-								</th>
-
-								<td className="px-6 py-4 text-right">
-									<a
-										href="#"
-										className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-									>
-										Edit
-									</a>
-								</td>
-							</tr>
+							
 							<tr className="bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600">
 								<th
 									scope="row"
