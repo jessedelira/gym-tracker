91e0af9187510a8aa5efea2ce5faf589a768e8aa
Jesse De Lira <jessedelira1@gmail.com>
2023-12-15 20:50:59 -0800

    Add setActiveRoutineMutation and onStarClick function

--- src/pages/manage/routines.tsx
+++ src/pages/manage/routines.tsx
@@ -17,6 +17,14 @@ const Routines: NextPage = () => {
 	const { data: activeRoutineData } = api.routine.getActiveRoutine.useQuery({
 		userId: sessionData?.user.id || '',
 	});
+	const setActiveRoutineMutation = api.routine.setActiveRoutine.useMutation();
+
+	const onStarClick = (routineId: string) => {
+		console.log('hello world!!!!!! ' + routineId);
+		setActiveRoutineMutation.mutate({
+			routineId: routineId,
+		});
+	};
 
 	useEffect(() => {
 		if (status === 'unauthenticated') {
@@ -40,7 +48,7 @@ const Routines: NextPage = () => {
 					</h1>
 					<div className="flex justify-between ">
 						{activeRoutineData ? (
-							<p className="w-120 w-48 flex-initial pl-2 pt-1 text-s">
+							<p className="w-120 text-s w-48 flex-initial pl-2 pt-1">
 								Active Routine: {activeRoutineData.name}
 							</p>
 						) : (
@@ -49,11 +57,9 @@ const Routines: NextPage = () => {
 							</p>
 						)}
 
-
-						
 						<Link
 							href="/create/routine"
-							className="h-7 w-7 flex-none mr-2"
+							className="mr-2 h-7 w-7 flex-none"
 						>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -70,9 +76,6 @@ const Routines: NextPage = () => {
 								/>
 							</svg>
 						</Link>
-
-
-
 					</div>
 				</div>
 
@@ -120,9 +123,7 @@ const Routines: NextPage = () => {
 
 											<td className="grid grid-cols-2 px-6 py-4 text-right">
 												{routine.isActive ? (
-													// Case when routine is active
-													<a
-														href="#"
+													<button
 														className="font-medium text-yellow-400 hover:underline dark:text-yellow-400"
 													>
 														<svg
@@ -139,21 +140,28 @@ const Routines: NextPage = () => {
 																d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
 															/>
 														</svg>
-													</a>
+													</button>
 												) : (
-													// Case when routine is not active
-													<svg
-														xmlns="http://www.w3.org/2000/svg"
-														viewBox="0 0 24 24"
-														fill="currentColor"
-														className="h-6 w-6"
+													<button
+														onClick={() =>
+															onStarClick(
+																routine.id,
+															)
+														}
 													>
-														<path
-															fillRule="evenodd"
-															d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-															clipRule="evenodd"
-														/>
-													</svg>
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 24 24"
+															fill="currentColor"
+															className="h-6 w-6"
+														>
+															<path
+																fillRule="evenodd"
+																d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+																clipRule="evenodd"
+															/>
+														</svg>
+													</button>
 												)}
 												<svg
 													xmlns="http://www.w3.org/2000/svg"
