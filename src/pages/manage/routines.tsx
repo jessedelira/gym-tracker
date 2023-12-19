14850d02366c406e9b54cf15b5d8304b62852c8a
Jesse De Lira <jessedelira1@gmail.com>
2023-12-18 21:09:04 -0800

    Add star icons for active and inactive routines

--- src/pages/manage/routines.tsx
+++ src/pages/manage/routines.tsx
@@ -1,3 +1,4 @@
+import { type Routine } from '@prisma/client';
 import { type NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -5,10 +6,13 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import NavBar from '~/components/navbar';
 import { api } from '~/utils/api';
+import GoldStar from '~/components/goldStar';
+import GreyStar from '~/components/greyStar';
 
 const Routines: NextPage = () => {
 	const { data: sessionData, status } = useSession();
 	const [isLoading, setIsLoading] = useState(true);
+	const [activeRoutine, setActiveRoutine] = useState<Routine | null>(null);
 	const router = useRouter();
 
 	const { data: routineData } = api.routine.getRoutines.useQuery({
@@ -20,10 +24,15 @@ const Routines: NextPage = () => {
 	const setActiveRoutineMutation = api.routine.setActiveRoutine.useMutation();
 
 	const onStarClick = (routineId: string) => {
-		console.log('hello world!!!!!! ' + routineId);
 		setActiveRoutineMutation.mutate({
 			routineId: routineId,
 		});
+		const clickedRoutine = routineData?.find(
+			(routine) => routine.id === routineId,
+		);
+		if (clickedRoutine) {
+			setActiveRoutine(clickedRoutine);
+		}
 	};
 
 	useEffect(() => {
@@ -33,8 +42,15 @@ const Routines: NextPage = () => {
 			setIsLoading(true);
 		} else {
 			setIsLoading(false);
+			// set active routine
+			const currentActiveRoutine = activeRoutineData;
+			console.log('here');
+			if (currentActiveRoutine) {
+				console.log('here2');
+				setActiveRoutine(currentActiveRoutine);
+			}
 		}
-	}, [status, router]);
+	}, [status, router, activeRoutineData]);
 
 	if (isLoading) {
 		return <></>;
@@ -47,9 +63,9 @@ const Routines: NextPage = () => {
 						Manage Routines
 					</h1>
 					<div className="flex justify-between ">
-						{activeRoutineData ? (
+						{activeRoutine ? (
 							<p className="w-120 text-s w-48 flex-initial pl-2 pt-1">
-								Active Routine: {activeRoutineData.name}
+								Active Routine: {activeRoutine.name}
 							</p>
 						) : (
 							<p className="w-120 pl-2 pt-1 text-xl">
@@ -114,30 +130,19 @@ const Routines: NextPage = () => {
 														(routine.description
 															?.length > 40
 															? routine.description?.substring(
-																	0,
-																	40,
-															  ) + '...'
+																0,
+																40,
+															) + '...'
 															: routine.description)}
 												</p>
 											</th>
 
 											<td className="grid grid-cols-2 px-6 py-4 text-right">
-												{routine.isActive ? (
+												{activeRoutine &&
+													routine.id ===
+													activeRoutine.id ? (
 													<button className="font-medium text-yellow-400 hover:underline dark:text-yellow-400">
-														<svg
-															xmlns="http://www.w3.org/2000/svg"
-															fill="none"
-															viewBox="0 0 24 24"
-															strokeWidth={1.5}
-															stroke="currentColor"
-															className="h-6 w-6"
-														>
-															<path
-																strokeLinecap="round"
-																strokeLinejoin="round"
-																d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-															/>
-														</svg>
+														<GoldStar />
 													</button>
 												) : (
 													<button
@@ -147,18 +152,7 @@ const Routines: NextPage = () => {
 															)
 														}
 													>
-														<svg
-															xmlns="http://www.w3.org/2000/svg"
-															viewBox="0 0 24 24"
-															fill="currentColor"
-															className="h-6 w-6"
-														>
-															<path
-																fillRule="evenodd"
-																d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-																clipRule="evenodd"
-															/>
-														</svg>
+														<GreyStar />
 													</button>
 												)}
 												<svg
