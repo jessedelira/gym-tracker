afea4ce478b0ef5dbc76a27eb5301d36040de152
Jesse De Lira <jessedelira1@gmail.com>
2023-12-18 21:35:52 -0800

    Add height utility classes to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from 'tailwindcss';
 export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			height: {
+				'110' : '28rem', // 448px
+				'128' : '32rem', // 512px
+				'144' : '36rem', // 576px
+				'160' : '40rem', // 640px
+				'176' : '44rem', // 704px
+				'186' : '46.5rem', // 744px
+				'192' : '48rem', // 768px
+
+		},
 	},
 	plugins: [],
-} satisfies Config;
+}} satisfies Config;
