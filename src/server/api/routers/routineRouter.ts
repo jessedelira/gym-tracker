d0b2fcd6d3d6b0933c03b06b42c79a2279bfece5
Jesse De Lira <jessedelira1@gmail.com>
2023-12-20 17:28:54 -0800

    Add routineRouter and remove userRouter

---
+++ src/server/api/routers/routineRouter.ts
@@ -0,0 +1,78 @@
+import { z } from 'zod';
+import { createTRPCRouter, protectedProcedure } from '~/server/api/trpc';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const routineRouter = createTRPCRouter({
+	createRoutine: protectedProcedure
+		.input(
+			z.object({
+				name: z.string(),
+				description: z.string(),
+				userId: z.string(),
+			}),
+		)
+		.mutation(({ input }) => {
+			const createdRoutine = prisma.routine.create({
+				data: {
+					name: input.name,
+					description: input.description,
+					userId: input.userId,
+				},
+			});
+
+			return createdRoutine;
+		}),
+
+	getRoutines: protectedProcedure
+		.input(z.object({ userId: z.string() }))
+		.query(({ input }) => {
+			const routines = prisma.routine.findMany({
+				where: {
+					userId: input.userId,
+				},
+			});
+
+			return routines;
+		}),
+
+	getActiveRoutine: protectedProcedure
+		.input(z.object({ userId: z.string() }))
+		.query(async ({ input }) => {
+			const routine = await prisma.routine.findFirst({
+				where: {
+					userId: input.userId,
+					isActive: true,
+				},
+			});
+
+			return routine;
+		}),
+
+	setActiveRoutine: protectedProcedure
+		.input(z.object({ routineId: z.string() }))
+		.mutation(async ({ input }) => {
+			// Set all routines to inactive, just in case
+			await prisma.routine.updateMany({
+				where: {
+					isActive: true,
+				},
+				data: {
+					isActive: false,
+				},
+			});
+
+			// Set the selected routine to active
+			const routine = await prisma.routine.update({
+				where: {
+					id: input.routineId,
+				},
+				data: {
+					isActive: true,
+				},
+			});
+
+			return routine;
+		}),
+});
