b77da120aeed753fb4b40ae2c9490f64a0777080
Jesse De Lira <jessedelira1@gmail.com>
2024-01-07 13:36:53 -0800

    Refactor create routine page layout

--- src/pages/create/routine.tsx
+++ src/pages/create/routine.tsx
@@ -65,58 +65,58 @@ const Routine: NextPage = () => {
 		return (
 			<>
 				<Layout sessionData={sessionData}>
-				<div className="flex flex-col">
-					<h1 className="pl-2 text-3xl font-bold">Create</h1>
+					<div className="flex flex-col">
+						<h1 className="pl-2 text-3xl font-bold">Create</h1>
 
-					<h2 className="pl-2 text-2xl font-bold">
-						Routine Information
-					</h2>
-					<form onSubmit={(e) => void handleSaveClicked(e)}>
-						<div className="mat-4 flex">
-							<div className="mat-4 w-18 mr-2 grid grid-cols-1 pl-2">
-								<label className="block font-bold">
-									Routine Name
+						<h2 className="pl-2 text-2xl font-bold">
+							Routine Information
+						</h2>
+						<form onSubmit={(e) => void handleSaveClicked(e)}>
+							<div className="mat-4 flex">
+								<div className="mat-4 w-18 mr-2 grid grid-cols-1 pl-2">
+									<label className="block font-bold">
+										Routine Name
+									</label>
+									<input
+										id="routineName"
+										className=" rounded-md bg-gray-300 px-4 py-2 text-white"
+										placeholder="Name"
+										onChange={handleInputChange}
+										required
+									></input>
+								</div>
+							</div>
+							<div className="mat-4 grid grid-cols-1">
+								<label className="block pl-2 font-bold">
+									Description
 								</label>
-								<input
-									id="routineName"
-									className=" rounded-md bg-gray-300 px-4 py-2 text-white"
-									placeholder="Name"
+								<textarea
+									id="routineDescription"
+									className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-white"
+									placeholder="Description"
 									onChange={handleInputChange}
 									required
-								></input>
+								></textarea>
 							</div>
-						</div>
-						<div className="mat-4 grid grid-cols-1">
-							<label className="block pl-2 font-bold">
-								Description
-							</label>
-							<textarea
-								id="routineDescription"
-								className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-white"
-								placeholder="Description"
-								onChange={handleInputChange}
-								required
-							></textarea>
-						</div>
 
-						{dataChangeInForm ? (
-							<div className="mt-4 grid grid-cols-2 gap-1">
-								<button
-									className="ml-2 rounded-md bg-green-700 px-4 py-2 text-white"
-									type="submit"
-								>
-									Save
-								</button>
-								<button
-									className="mr-2 rounded-md bg-red-700 px-4 py-2 text-white"
-									onClick={handleCancelClicked}
-								>
-									Cancel
-								</button>
-							</div>
-						) : null}
-					</form>
-				</div>
+							{dataChangeInForm ? (
+								<div className="mt-4 grid grid-cols-2 gap-1">
+									<button
+										className="ml-2 rounded-md bg-green-700 px-4 py-2 text-white"
+										type="submit"
+									>
+										Save
+									</button>
+									<button
+										className="mr-2 rounded-md bg-red-700 px-4 py-2 text-white"
+										onClick={handleCancelClicked}
+									>
+										Cancel
+									</button>
+								</div>
+							) : null}
+						</form>
+					</div>
 				</Layout>
 			</>
 		);
