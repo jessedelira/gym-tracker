f8ea58db3b8a854c6cc606fd7188a487d15aaeec
Jesse De Lira <jessedelira1@gmail.com>
2024-02-19 19:29:44 -0800

    Add workoutRouter to handle creating workouts

---
+++ src/server/api/routers/workoutRouter.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
+
+const primsa = new PrismaClient();
+
+export const workoutRouter = createTRPCRouter({
+	createWorkout: protectedProcedure
+		.input(
+			z.object({
+				exerciseId: z.string(),
+				weight: z.number(),
+				reps: z.number(),
+				sets: z.number(),
+			}),
+		)
+.mutation(({ input }) => {
+    const createdWorkout = primsa.workout.create({
+        data: {
+            exerciseId: input.exerciseId,
+            weight: input.weight,
+            reps: input.reps,
+            sets: input.sets,
+            sessionId: 'your-session-id', // Add the sessionId property here
+        },
+    });
+    return createdWorkout;
+}),
+});
