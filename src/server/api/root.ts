c72fda9e5a1dfcaa3ff00ad24a36a4f2b9ec19ec
Jesse De Lira <jessedelira1@gmail.com>
2024-02-19 19:46:30 -0800

    Add session and workout routers to appRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,8 @@ import { createTRPCRouter } from '~/server/api/trpc';
 import { userRouter } from './routers/userRouter';
 import { routineRouter } from './routers/routineRouter';
 import { exerciseRouter } from './routers/exerciseRouter';
+import { sessionRouter } from './routers/sessionRouter';
+import { workoutRouter } from './routers/workoutRouter';
 
 /**
  * This is the primary router for your server.
@@ -12,6 +14,8 @@ export const appRouter = createTRPCRouter({
 	user: userRouter,
 	routine: routineRouter,
 	exercise: exerciseRouter,
+	session: sessionRouter,
+	workout: workoutRouter,
 });
 
 // export type definition of API
