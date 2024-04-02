4b46264fe014469967f4fbe9dc4978253576b009
Jesse De Lira <jessedelira1@gmail.com>
2024-04-01 22:56:38 -0400

    Revert "Revert "adding the pwa files back""
    This reverts commit 2bae9274393643641adda4a11fe174ced374860e.

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,33 @@
-import next from "next";
-import nextPWA from "next-pwa";
+import nextPWA from 'next-pwa';
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
-const prod = process.env.NODE_ENV === "production";
-const withPWA = nextPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: prod ? false : true,
-});
+await import('./src/env.mjs');
+const prod = process.env.NODE_ENV === 'production';
 
+const withPWA = nextPWA({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	disable: prod ? false : true,
+});
 
 /** @type {import("next").NextConfig} */
 const config = withPWA({
-  reactStrictMode: true,
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  images: {
+	reactStrictMode: true,
+	/**
+	 * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+	 * out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
+	images: {
 		remotePatterns: [
 			{
 				protocol: 'https',
