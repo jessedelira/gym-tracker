11cd044c5d95d3b612d2942a73fc4e399de278dc
Jesse De Lira <jessedelira1@gmail.com>
2024-04-19 21:28:44 -0400

    styling nav icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,8 +5,8 @@ import ClockIcon from './clockIcon';
 
 const NavBar: React.FC<NavBarProps> = () => {
 	return (
-		<nav className="sticky bottom-0 flex h-[4rem] w-full flex-row items-center justify-between border-t-2 border-t-black bg-white">
-			<div className="flex flex-row items-center justify-between gap-1">
+		<nav className="sticky bottom-0 flex h-[5rem] w-full flex-row items-center justify-between border-t-2 border-t-black bg-white">
+			<div className="flex flex-row items-center justify-between gap-1 mb-2">
 				<Link href="/home" className="ml-3 rounded-full text-5xl">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -14,7 +14,7 @@ const NavBar: React.FC<NavBarProps> = () => {
 						viewBox="0 0 24 24"
 						strokeWidth="1.5"
 						stroke="currentColor"
-						className="h-6 w-6"
+						className="h-7 w-7"
 					>
 						<path
 							strokeLinecap="round"
@@ -25,7 +25,7 @@ const NavBar: React.FC<NavBarProps> = () => {
 				</Link>
 			</div>
 
-			<div className="flex flex-row items-center justify-between gap-1">
+			<div className="flex flex-row items-center justify-between gap-1 mb-2">
 				<Link href="/manage" className="ml-3 rounded-full text-5xl">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -33,7 +33,7 @@ const NavBar: React.FC<NavBarProps> = () => {
 						viewBox="0 0 24 24"
 						strokeWidth="1.5"
 						stroke="currentColor"
-						className="h-6 w-6"
+						className="h-7 w-7"
 					>
 						<path
 							strokeLinecap="round"
@@ -44,7 +44,7 @@ const NavBar: React.FC<NavBarProps> = () => {
 				</Link>
 			</div>
 
-			<div className="flex flex-row items-center justify-between gap-1">
+			<div className="flex flex-row items-center justify-between gap-1 mb-2">
 				<Link href="/settings" className="ml-3 rounded-full text-5xl">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -52,7 +52,7 @@ const NavBar: React.FC<NavBarProps> = () => {
 						viewBox="0 0 24 24"
 						strokeWidth="1.5"
 						stroke="currentColor"
-						className="h-6 w-6"
+						className="h-7 w-7"
 					>
 						<path
 							strokeLinecap="round"
@@ -68,13 +68,13 @@ const NavBar: React.FC<NavBarProps> = () => {
 				</Link>
 			</div>
 
-			<div className="flex flex-row items-center justify-between gap-1">
+			<div className="flex flex-row items-center justify-between gap-1 mb-2">
 				<Link href="/clock" className="ml-3 rounded-full text-5xl">
 					<ClockIcon />
 				</Link>
 			</div>
 
-			<div className="flex flex-row items-center justify-between gap-1">
+			<div className="flex flex-row items-center justify-between gap-1 mb-2">
 				<button
 					className=" mr-3 rounded-full text-5xl"
 					onClick={() => void signOut()}
@@ -85,7 +85,7 @@ const NavBar: React.FC<NavBarProps> = () => {
 						viewBox="0 0 24 24"
 						strokeWidth="1.5"
 						stroke="currentColor"
-						className="h-6 w-6"
+						className="h-7 w-7"
 					>
 						<path
 							strokeLinecap="round"
