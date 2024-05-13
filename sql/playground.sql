dde6da2f03168727b6840a020214b4a75e5d7bff
Jesse De Lira <jessedelira1@gmail.com>
2024-05-13 08:48:45 -0400

    chore: Add SQL query to retrieve sessions related to a specific user and not connected to the current active routine

---
+++ sql/playground.sql
@@ -0,0 +1,19 @@
+select *
+from session;
+
+select *
+from user;
+
+
+delete from session
+where id = 'clw49gnog0007hej88oe5wyyg';
+
+
+-- I want all of the sessions that are related to specific user and that are not connected to the current active routine
+select *
+from session as s
+left join routine as r
+on s.routine_id = r.id
+where s.user_id = 'clw3v31uo00005hyignh0oukl'
+AND s.routine_id IS NULL OR
+    s.routine_id != (select id from routine where r.is_active = true and r.id = 'clw3v31uo00005hyignh0oukl')
