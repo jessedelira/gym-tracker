301ee571ae1409d5eca3fe5f8f3f1bf3590a92e5
Jesse De Lira <jessedelira1@gmail.com>
2024-05-23 21:45:31 -0400

    refactor: Remove global prisma instance in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,11 @@ import seedUsers from './seeds/userSeeds';
 import seedExercises from './seeds/exerciseSeeds';
 
 
+/**
+ * Cannot use global prisma instance in seed files because of the server not running yet,
+ * the prisma global references an instance of PrismaClient that is connected to the database. But 
+ * that doesn't happen when the npm i/seed commands are executed
+ */
 const main = async () => {
 	await seedUsers();
 	await seedExercises();
