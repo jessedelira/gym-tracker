f6005ea30ed1ebe5d2588db9396175302612bbd0
Jesse De Lira <jessedelira1@gmail.com>
2024-05-27 16:46:08 -0400

    chore: Add missing JSX closing tags in home, manage, and settings pages

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -23,7 +23,9 @@ const Settings: NextPage = () => {
 	}, [status, router, sessionData?.user]);
 
 	if (isLoading) {
+		<Layout sessionData={sessionData ? sessionData : null}>
 			<Spinner />
+		</Layout>;
 	} else {
 		return (
 			<>
