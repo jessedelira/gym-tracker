f9ddbf510f9c86d9cbd2e588cb74de204a501104
Jesse De Lira <jessedelira1@gmail.com>
2024-05-27 21:18:36 -0400

    refactor: Remove unnecessary border in main layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import NavBar from './navbar';
 const Layout: React.FC<LayoutProps> = ({ sessionData, children }) => {
 	return (
 		<div className='flex flex-col h-screen'>
-			<main className="flex flex-col h-full bg-white border-red-600 border-2">{children}</main>
+			<main className="flex flex-col h-full bg-white">{children}</main>
 			<NavBar sessionData={sessionData ? sessionData : null}></NavBar>
 		</div>
 	);
