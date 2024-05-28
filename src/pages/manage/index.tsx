54c1afa1737545fdda6347e39f64d0ac620aded9
Jesse De Lira <jessedelira1@gmail.com>
2024-05-27 21:12:33 -0400

    refactor: Remove commented out code and unused component in manage page

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -33,8 +33,6 @@ const Manage: NextPage = () => {
 		</Layout>;
 	} else {
 		return (
-			// create three buttons: create workout, create exercise, create routine with tailwind css
-
 				<Layout sessionData={sessionData}>
 					<div className="flex items-center">
 						<div className="mt-4 pl-4">
@@ -91,7 +89,7 @@ const Manage: NextPage = () => {
 								currently following."
 							/>
 						</div>
-						<ActivityGraph />
+						{/* <ActivityGraph /> */}
 					</div>
 					<div className="mt-6"></div>
 				</Layout>
