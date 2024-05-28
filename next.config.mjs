0a3336ab0a96e34c119a2230d6fcb72bba225bd7
Jesse De Lira <jessedelira1@gmail.com>
2024-05-27 21:55:01 -0400

    refactor: Remove unused sessionData prop from AccountDeletion, Home, and Layout components, and unnecessary border in main layout

--- next.config.mjs
+++ next.config.mjs
@@ -18,16 +18,6 @@ const withSerwist = withSerwistInit({
 /** @type {import("next").NextConfig} */
 const config = withSerwist({
   reactStrictMode: true,
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   images: {
 		remotePatterns: [
 			{
