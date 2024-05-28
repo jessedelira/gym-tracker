1e66128a465ac04c60d18774123a6ce70cc71bdc
Jesse De Lira <jessedelira1@gmail.com>
2024-05-27 21:34:06 -0400

    refactor: Remove unnecessary sessionData prop from Layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import type LayoutProps from '~/interfaces/layoutProps';
 import NavBar from './navbar';
 
-const Layout: React.FC<LayoutProps> = ({ sessionData, children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<div className='flex flex-col h-screen'>
 			<main className="flex flex-col h-full bg-white">{children}</main>
