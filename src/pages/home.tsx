80b52b8fa20e13bd00b76a1d9525f8e8040e581e
Jesse De Lira <jessedelira1@gmail.com>
2024-05-29 22:41:33 -0400

    refactor: Update next.js version to 14.0.1 and optimize navbar links

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,22 @@
 import { type NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import Spinner from '~/components/Spinner';
 import CurrentWorkoutDisplay from '~/components/currentWorkoutDisplay';
 import Layout from '~/components/layout';
 
 const Home: NextPage = () => {
-	const { status } = useSession();
-	const [isLoading, setIsLoading] = useState(true);
+	const { data: sessionData, status } = useSession();
 	const router = useRouter();
-	console.log(isLoading);
 	useEffect(() => {
 		if (status === 'unauthenticated') {
 			void router.push('/');
-		} else if (status === 'loading') {
-			setIsLoading(true);
-		} else {
-			setIsLoading(false);
 		}
 	}, [status, router]);
 
-	if(isLoading === true) {
-		return null;
+	if(!sessionData) {
+		return <Spinner/>;
 	}
 
 	return (
