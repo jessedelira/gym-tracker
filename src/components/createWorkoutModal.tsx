0129c764905c45e01aa6a55a9a9dae5b57b1cb4e
Jesse De Lira <jessedelira1@gmail.com>
2024-06-07 18:30:16 -0400

    chore: Format file and adjust margin-top in ActivityGraph component

--- src/components/createWorkoutModal.tsx
+++ src/components/createWorkoutModal.tsx
@@ -1,9 +1,10 @@
-import React, { FormEvent } from 'react';
+import React, { type FormEvent } from 'react';
 import XIcon from './icons/xIcon';
 import { api } from '~/utils/api';
 
 interface CreateWorkoutModalProps {
 	onXClick: () => void;
+	onSaveClick: () => void;
 }
 
 const CreateWorkoutModal: React.FC<CreateWorkoutModalProps> = ({
@@ -23,26 +24,27 @@ const CreateWorkoutModal: React.FC<CreateWorkoutModalProps> = ({
 		<>
 			<div className="fixed inset-0 z-10 flex items-center justify-center">
 				<div className="absolute inset-0 bg-black opacity-50"></div>
-
-				<div className="z-10 max-w-[23rem] rounded-lg bg-white p-16">
+				<div className="z-10 max-w-[23rem] rounded-lg bg-white p-2">
 					<div className="flex flex-col justify-start">
-						<button onClick={() => onXClick()}>
+						<button  onClick={() => onXClick()}>
+							
 							<XIcon></XIcon>
+							
 						</button>
 					</div>
 					<h1 className="text-center text-2xl text-black">
 						Create Workout
 					</h1>
 					<form onSubmit={(e) => void handleSaveClicked(e)}>
-						<div className="mat-4 flex">
-							<div className="mat-4 w-18 mr-2 grid grid-cols-1 pl-2">
+						<div className="flex">
+							<div className=" mr-2 grid grid-cols-1 pl-2">
 								<label className="block font-bold">
 									Exercise
 								</label>
 								<select
 									id="exerciseId"
 									required
-									className="rounded-md bg-gray-300  py-2 text-white"
+									className="rounded-md bg-gray-300  py-2 text-black"
 								>
 									{exercises
 										? exercises.data?.map((exercise) => (
@@ -56,13 +58,13 @@ const CreateWorkoutModal: React.FC<CreateWorkoutModalProps> = ({
 										: null}
 								</select>
 							</div>
-							<div className="mat-4 grid grid-cols-1">
+							<div className=" grid grid-cols-1">
 								<label className="block pl-2 font-bold">
 									Weight (lbs)
 								</label>
 								<input
 									id="weightLbs"
-									className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-white"
+									className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-black"
 									placeholder="75"
 									onChange={handleInputChange}
 									required
@@ -70,30 +72,40 @@ const CreateWorkoutModal: React.FC<CreateWorkoutModalProps> = ({
 							</div>
 						</div>
 						<div className="flex">
-							<div className="mat-4 grid grid-cols-1">
-								<label className="block pl-2 font-bold">
+							<div className="grid grid-cols-1">
+							<label className="block pl-2 font-bold">
+									Sets
+								</label>
+ 								<input
+									id="sets"
+									className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-black"
+									placeholder="3"
+									onChange={handleInputChange}
+									required
+								></input>
+								
+							</div>
+							<div className="grid grid-cols-1">
+							<label className="block pl-2 font-bold">
 									Reps
 								</label>
 								<input
 									id="reps"
-									className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-white"
+									className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-black"
 									placeholder="10"
 									onChange={handleInputChange}
 									required
 								></input>
 							</div>
-							<div className="mat-4 grid grid-cols-1">
-								<label className="block pl-2 font-bold">
-									Sets
-								</label>
-								<input
-									id="sets"
-									className="mx-2 rounded-md bg-gray-300 px-4 py-2 text-white"
-									placeholder="3"
-									onChange={handleInputChange}
-									required
-								></input>
-							</div>
+						</div>
+
+						<div className="flex justify-center mt-2">
+							<button
+								type="submit"
+								className="bg-green-400 text-black rounded-md px-4 py-2"
+							>
+								Save
+							</button>
 						</div>
 					</form>
 				</div>
