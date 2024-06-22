1d6d461b052062395fde4f764dc580aa20bc17f7
Jesse De Lira <jessedelira1@gmail.com>
2024-06-21 22:26:18 -0400

    add custom style to home page card component

--- src/components/homePageSessionCard.tsx
+++ src/components/homePageSessionCard.tsx
@@ -12,7 +12,7 @@ const HomePageSessionCard: React.FC<HomePageSessionCardProps> = ({
 	return (
 		<section className="bg-muted w-full py-12 md:py-24 lg:py-32">
 			<div className="container grid grid-cols-1 gap-6 px-4 md:grid-cols-3 md:px-6">
-				<div className="overflow-hidden rounded-lg border">
+				<div className="overflow-hidden rounded-lg border-2">
 					<div className="p-4">
 						<h3 className="text-xl font-bold">{sessionName}</h3>
 						<p className="text-muted-foreground">
@@ -21,7 +21,7 @@ const HomePageSessionCard: React.FC<HomePageSessionCardProps> = ({
 						<div className="mt-4">
 							<button
 								onClick={handleStartButtonClick}
-								className="bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:ring-ring inline-flex h-9 items-center justify-center rounded-md px-4 py-2 text-sm font-medium shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
+								className="bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:ring-ring inline-flex h-9 items-center justify-center rounded-md bg-blue-400 px-4 py-2 text-sm font-medium shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
 							>
 								Start
 							</button>
