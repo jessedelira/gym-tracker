91c54460cd82e415b584732986768a1805197d84
Jesse De Lira <jessedelira1@gmail.com>
2024-06-22 08:12:32 -0400

    remove error causing router skeleton code

--- src/server/api/routers/completedSessionRouter.ts
+++ src/server/api/routers/completedSessionRouter.ts
@@ -1,5 +1,1 @@
-import { prisma } from '~/server/db';
-import { z } from 'zod';
-import { createTRPCRouter, protectedProcedure } from '../trpc';
 
-export const completedSessionRouter = createTRPCRouter({});
