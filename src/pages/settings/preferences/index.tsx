bfd519b8ed5cd87ca9ad3891f6a3e9ed0a826f60
Jesse De Lira <jessedelira1@gmail.com>
2024-07-28 15:28:48 -0400

    prefrences page added

---
+++ src/pages/settings/preferences/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Layout from "~/components/layout";
+
+
+const Preferences: NextPage = () => {
+
+
+    return (
+        <Layout>
+        <div>
+            <h1>Preferences</h1>
+        </div>
+        </Layout>
+    )
+
+};
+
+export default Preferences;
