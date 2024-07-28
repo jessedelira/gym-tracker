b637405cc181467fbdee61b311ff5be28ceec35e
Jesse De Lira <jessedelira1@gmail.com>
2024-07-28 15:20:27 -0400

    add preferences on creation

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -108,6 +108,27 @@ const Settings: NextPage = () => {
 						</p>
 					</div>
 				</Link>
+				<Link href="/settings/language">
+					<div className="flex h-8 border-b-[0.5px] border-black">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="ml-0.5 h-9 w-7 flex-none pb-1"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802"
+							/>
+						</svg>
+						<p className="h-9 w-14 flex-initial  pl-3 text-2xl">
+							Prefernces
+						</p>
+					</div>
+				</Link>
 				<Link href="measure">
 					<div className="flex h-8 border-b-[0.5px] border-black">
 						<svg
