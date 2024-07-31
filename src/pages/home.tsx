64dd435fd3671e256f21094851b320c3bdb51e15
Jesse De Lira <jessedelira1@gmail.com>
2024-07-31 17:14:40 -0400

    chore: Update Home component to pass user object instead of userId
    The code changes update the Home component to pass the user object instead of just the userId. This ensures that the component has access to all the necessary user data.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
 		<Layout>
 			<div className="flex h-full flex-col items-center bg-white">
 				<CurrentWorkoutDisplay
-					userId={sessionData.user.id}
+					user={sessionData.user}
 					currentDate={currentDate}
 				></CurrentWorkoutDisplay>
 			</div>
