6db898002f12be4c81562d7e89297742a7c6b328
alienware96 <sachin.a.perera@gmail.com>
2024-08-18 01:11:21 +1000

    new homepage as requested

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,11 +25,58 @@ const Home: NextPage = () => {
 	} else {
 		return (
 			<>
-				<main className="flex min-h-screen flex-col items-center justify-center bg-white">
-					<h1 className="mb-5 text-center text-2xl text-black">
-						Gym Tracker
-					</h1>
-					<AuthDisplay sessionData={sessionData}></AuthDisplay>
+				<main className="mt-[10%] flex min-h-screen min-w-full flex-col items-center bg-white">
+					<section className="min-w-full">
+						<div className="grid grid-cols-2">
+							<div className="p-10 text-right">
+								<h1 className="mb-5 text-5xl text-black">
+									Gym Tracker
+								</h1>
+								<AuthDisplay
+									sessionData={sessionData}
+								></AuthDisplay>
+							</div>
+							<div className="min-h-8 p-10">
+								<div className=" max-w-[50%] bg-gray-100 px-5">
+									<p className="pb-8 pt-4">
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit. Sed ultricies porta
+										nisl. Etiam porta porta lacus, sodales
+										mollis turpis pellentesque ut. Phasellus
+										urna lectus, sodales non nisi at,
+										viverra volutpat velit. In quis dapibus
+										tortor. Phasellus eget consectetur orci.
+										Integer volutpat ac enim ac sodales.
+										Quisque in lorem laoreet, viverra augue
+										consectetur, porta mauris. Donec ut
+										tincidunt turpis. Sed pellentesque
+										lectus scelerisque dolor fringilla, ac
+										sagittis velit laoreet.
+									</p>
+								</div>
+							</div>
+						</div>
+					</section>
+					<section>
+						<div className="min-w-full">
+							<div className="mb-8 grid">
+								<h1 className="text-center text-3xl">
+									You can track
+								</h1>
+							</div>
+							<div className="grid grid-cols-3 gap-12">
+								<div className="bg-gray-200 p-8">
+									Manage Routines
+								</div>
+								<div className="bg-gray-200 p-8">
+									Manage Sessions
+								</div>
+								<div className="bg-gray-200 p-8">
+									Active Routines
+								</div>
+							</div>
+						</div>
+					</section>
 				</main>
 			</>
 		);
