342fce638ba42e491f54a8a3fbcd035765c4e744
noelbarrera <70553615+noelbarrera@users.noreply.github.com>
2024-10-15 23:22:50 -0400

    remove dark theme from spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -8,7 +8,7 @@ const Spinner: React.FC = () => {
 		>
 			<svg
 				aria-hidden="true"
-				className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+				className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200"
 				viewBox="0 0 100 101"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
