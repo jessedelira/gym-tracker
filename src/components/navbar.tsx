e298dd0437000bac502305ae781e9202bc02c9e8
Jesse De Lira <jessedelira1@gmail.com>
2024-11-26 14:23:40 -0500

    manage now working in ui

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import ClockIcon from './icons/clockIcon';
 import { useEffect, useState } from 'react';
 import HomeIcon from './icons/homeIcon';
+import ManageIcon from './icons/manageIcon';
 
 const NavBar: React.FC = () => {
 	const [currentURL, setCurrentURL] = useState<string>('');
@@ -60,20 +61,21 @@ const NavBar: React.FC = () => {
 			</div>
 			<div id="manage" className="mb-8 flex flex-row items-center">
 				<Link href="/manage" className="ml-3 rounded-full text-5xl">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="black"
-						className="h-8 w-8"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"
+					{currentURL === 'manage' ? (
+						<ManageIcon
+							heightValue="8"
+							widthValue="8"
+							fill="none"
+							strokeColor="black"
 						/>
-					</svg>
+					) : (
+						<ManageIcon
+							heightValue="8"
+							widthValue="8"
+							fill="none"
+							strokeColor="gray"
+						/>
+					)}
 				</Link>
 			</div>
 			<div id="settings" className="mb-8 flex flex-row items-center ">
