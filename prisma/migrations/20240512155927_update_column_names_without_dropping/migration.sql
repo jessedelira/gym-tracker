508b12e14d603b7839c36d7ff75666ea31ffce2c
Jesse De Lira <jessedelira1@gmail.com>
2024-12-01 22:37:21 -0500

    refactor: rename columns in routine, session, session_days_active, and workout tables for consistency and clarity

---
+++ prisma/migrations/20240512155927_update_column_names_without_dropping/migration.sql
@@ -0,0 +1,72 @@
+/*
+  Warnings:
+
+  - The column name `createdAt` on the `routine` table would be renamed to `created_at`
+  - The column name `isActive` on the `routine` table would be renamed to `is_active`
+  - The column name `userId` on the `routine` table would be renamed to `user_id`
+  - The column name `routineId` on the `session` table would be renamed to `routine_id`
+  - The column name `userId` on the `session` table would be renamed to `user_id`
+  - The column name `sessionId` on the `session_days_active` table would be renamed to `session_id`
+  - The column name `exerciseId` on the `workout` table would be renamed to `exercise_id`
+  - The column name `sessionId` on the `workout` table would be renamed to `session_id`
+  - The column name `userId` on the `workout` table would be renamed to `user_id`
+*/
+
+-- DropForeignKey
+ALTER TABLE `routine` DROP FOREIGN KEY `routine_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `session_routineId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `session_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session_days_active` DROP FOREIGN KEY `session_days_active_sessionId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `workout` DROP FOREIGN KEY `workout_exerciseId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `workout` DROP FOREIGN KEY `workout_sessionId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `workout` DROP FOREIGN KEY `workout_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `routine` RENAME COLUMN `createdAt` TO `created_at`,
+    RENAME COLUMN `isActive` TO `is_active`,
+    RENAME COLUMN `userId` TO `user_id`;
+
+-- AlterTable
+ALTER TABLE `session` RENAME COLUMN `routineId` TO `routine_id`,
+    RENAME COLUMN `userId` TO `user_id`;
+
+-- AlterTable
+ALTER TABLE `session_days_active` RENAME COLUMN `sessionId` TO `session_id`;
+
+-- AlterTable
+ALTER TABLE `workout` RENAME COLUMN `exerciseId` TO `exercise_id`,
+    RENAME COLUMN `sessionId` TO `session_id`,
+    RENAME COLUMN `userId` TO `user_id`;
+
+-- AddForeignKey
+ALTER TABLE `routine` ADD CONSTRAINT `routine_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `session` ADD CONSTRAINT `session_routine_id_fkey` FOREIGN KEY (`routine_id`) REFERENCES `routine`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `session` ADD CONSTRAINT `session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `session_days_active` ADD CONSTRAINT `session_days_active_session_id_fkey` FOREIGN KEY (`session_id`) REFERENCES `session`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `workout` ADD CONSTRAINT `workout_exercise_id_fkey` FOREIGN KEY (`exercise_id`) REFERENCES `exercise`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `workout` ADD CONSTRAINT `workout_session_id_fkey` FOREIGN KEY (`session_id`) REFERENCES `session`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `workout` ADD CONSTRAINT `workout_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
