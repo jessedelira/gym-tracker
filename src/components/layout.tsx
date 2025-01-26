9b13f64e956b837d05100806a31495bf8cb2e488
Jesse De Lira <jessedelira1@gmail.com>
2025-01-25 21:13:37 -0500

    use margin at the top of main instead of padding

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<div className="flex h-[100dvh] w-full flex-col overflow-hidden bg-white">
-			<main className="flex-1 overflow-y-auto pt-4 pb-20">{children}</main>
+			<main className="flex-1 overflow-y-auto mt-4 pb-20">{children}</main>
 			<NavBar />
 		</div>
 	);
