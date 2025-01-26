94af331b0361c40572110423c71cdfe9af04d7ce
Jesse De Lira <jessedelira1@gmail.com>
2025-01-25 23:08:18 -0500

    padding

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<div className="flex h-[100dvh] w-full flex-col overflow-hidden bg-white">
-			<main className="flex-1 overflow-y-auto mb-24 pt-4">
+			<main className="flex-1 overflow-y-auto mb-24 pt-16">
 				{children}
 			</main>
 			<NavBar />
