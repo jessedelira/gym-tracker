f57f19c133efb84ce1d206c15331e3920abfb237
Jesse De Lira <jessedelira1@gmail.com>
2025-03-29 16:36:35 -0500

    remove the navbar log out and clock, update the UI of the list of settings page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,3 @@
-import { signOut } from 'next-auth/react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import HomeIcon from './icons/homeIcon';
@@ -80,26 +79,6 @@ const NavBar: React.FC = () => {
 					/>
 				)}
 			</Link>
-
-			<button
-				className="rounded-full pb-4 text-5xl"
-				onClick={() => void signOut()}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth="1.5"
-					stroke="red"
-					className="h-8 w-8"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-					/>
-				</svg>
-			</button>
 		</nav>
 	);
 };
