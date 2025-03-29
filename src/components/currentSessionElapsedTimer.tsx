27a169f9c9727ff6bb8faf34adb284740abccae5
Jesse De Lira <jessedelira1@gmail.com>
2025-03-29 17:20:28 -0500

    update timer to not show undefined at the start of a workout

--- src/components/currentSessionElapsedTimer.tsx
+++ src/components/currentSessionElapsedTimer.tsx
@@ -9,11 +9,19 @@ interface CurrentSessionElapsedTimerProps {
 const CurrentWorkoutDisplay: React.FC<CurrentSessionElapsedTimerProps> = ({
 	startedAtDate,
 }) => {
-	const [elapsedMinutes, setElapsedMinutes] = useState<string | null>();
-	const [elapsedSeconds, setElapsedSeconds] = useState<string | null>();
+	const [elapsedMinutes, setElapsedMinutes] = useState<string>("0");
+	const [elapsedSeconds, setElapsedSeconds] = useState<string>("0");
 	const { data: sessionData } = useSession();
 
 	useEffect(() => {
+		const currentTime = new Date();
+		const elapsedMilliseconds = currentTime.getTime() - startedAtDate.getTime();
+		const initialMinutes = Math.floor(elapsedMilliseconds / 60000);
+		const initialSeconds = Math.floor((elapsedMilliseconds % 60000) / 1000);
+		
+		setElapsedMinutes(`${initialMinutes}`);
+		setElapsedSeconds(`${initialSeconds}`);
+
 		const interval = setInterval(() => {
 			const currentTime = new Date();
 
