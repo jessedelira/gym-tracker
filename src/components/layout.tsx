3488d2122d3fc744de66b76c91be8fe13cf6f464
Jesse De Lira <jessedelira1@gmail.com>
2025-04-08 11:35:27 -0500

    Update Layout component to add top padding for improved spacing in the main content area.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<div className="flex h-[100dvh] w-full flex-col overflow-hidden">
-			<main className="flex-1 overflow-y-auto bg-gray-50 ">
+			<main className="flex-1 overflow-y-auto bg-gray-50 pt-10">
 				{children}
 			</main>
 			<nav>
