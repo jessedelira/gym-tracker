b74c244817349fa4ba115d3d089d9952034fbc15
Jesse De Lira <jessedelira1@gmail.com>
2025-04-26 22:39:24 -0500

    update createUser method to handle creation of elapsed seconds preference for new users

--- src/server/api/routers/userRouter.ts
+++ src/server/api/routers/userRouter.ts
@@ -45,6 +45,13 @@ export const userRouter = createTRPCRouter({
 				},
 			});
 
+			await prisma.userPreference.create({
+				data: {
+					preference: Preference.SHOW_ELAPSED_SECONDS_IN_ACTIVE_SESSION,
+					userId: createdUser.id
+				}
+			})
+
 			await prisma.userSetting.create({
 				data: {
 					timezoneId: input.timezoneId,
